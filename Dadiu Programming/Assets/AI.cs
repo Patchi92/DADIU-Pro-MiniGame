04de1a7fa00d4362cda30b5b982ca9fa589201ad
Patchi <lechieu@hotmail.com>
2016-10-21 17:24:23 +0200

    Working on Spawn + AI - Kinda works :D:D:D

---
+++ Dadiu Programming/Assets/AI.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI : MonoBehaviour {
+
+    // Use this for initialization
+
+    NavMeshAgent carAI;
+    public GameObject goal;
+
+    bool passive;
+    bool hostile;
+
+    GameObject player;
+
+    public float ignoreDistance;
+
+    float moveSpeed;
+    float accelerationSpeed;
+    float maxSpeed;
+
+    void Awake ()
+    {
+        carAI = gameObject.GetComponent<NavMeshAgent>();
+        maxSpeed = 20;
+        accelerationSpeed = 0.3f;
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        goal = GameObject.FindGameObjectWithTag("Goal");
+
+        passive = true;
+        hostile = false;
+    }
+
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if(passive)
+        {
+            if (moveSpeed < maxSpeed && moveSpeed >= -6 && moveSpeed < 10)
+            {
+                moveSpeed = moveSpeed + accelerationSpeed;
+            }
+
+            if (moveSpeed < maxSpeed && moveSpeed >= 10 && moveSpeed < 15)
+            {
+                moveSpeed = moveSpeed + accelerationSpeed - 0.15f;
+            }
+
+            if (moveSpeed < maxSpeed && moveSpeed >= 15 && moveSpeed < 20)
+            {
+                moveSpeed = moveSpeed + accelerationSpeed - 0.25f;
+            }
+
+            carAI.speed = moveSpeed;
+            carAI.SetDestination(goal.transform.position);
+        }
+
+        if(hostile)
+        {
+            if (Vector3.Distance(gameObject.transform.position, player.transform.position) > ignoreDistance)
+            {
+                passive = true;
+                hostile = false;
+            }
+
+            if (moveSpeed < maxSpeed && moveSpeed >= -6 && moveSpeed < 10)
+            {
+                moveSpeed = moveSpeed + accelerationSpeed;
+            }
+
+            if (moveSpeed < maxSpeed && moveSpeed >= 10 && moveSpeed < 15)
+            {
+                moveSpeed = moveSpeed + accelerationSpeed - 0.15f;
+            }
+
+            if (moveSpeed < maxSpeed && moveSpeed >= 15 && moveSpeed < 20)
+            {
+                moveSpeed = moveSpeed + accelerationSpeed - 0.25f;
+            }
+
+            carAI.speed = moveSpeed;
+            carAI.SetDestination(player.transform.position);
+        }
+       
+
+
+
+	}
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            //hostile = true;
+            //passive = false;
+        }
+        
+    }
+
+    
+
+}
---
+++ Dadiu Programming/Assets/Spawn.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject carAI;
+
+    public int cars;
+
+    public Transform[] startPos;
+    public int players;
+
+	// Use this for initialization
+	void Start () {
+        SpawnCars();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void SpawnCars ()
+    {
+        Instantiate(player, startPos[0].position, Quaternion.identity);
+        players = players - 1;
+
+        for (int i = 0; i < players; i++)
+        {
+            Instantiate(carAI, startPos[i + 1].position, Quaternion.identity);
+        }
+    }
+}
